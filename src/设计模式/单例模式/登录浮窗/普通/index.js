988be3d526f56c4bdcb69b8566c4b9742c96acd5
张凡 <zhangfan@zhangfandeMacBook-Pro.local>
2021-05-30 12:19:58 +0800

    单例模式

---
+++ src/设计模式/单例模式/登录浮窗/普通/index.js
@@ -0,0 +1,13 @@
+var loginLayer = (() => {
+	let div = document.createElement('div')
+	div.innerHTML = '我是登录弹窗'
+	div.style.display = 'none'
+
+	document.body.appendChild(div)
+
+	return div
+})()
+
+document.getElementById('loginBtn').onclick = () => {
+	loginLayer.style.display = 'block'
+}
