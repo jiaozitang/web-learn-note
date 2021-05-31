ccf1868076f6f1b079604d20c3850510b3dcf0db
tangjiao12 <tangjiao11@jd.com>
2021-05-31 12:34:50 +0800

    设计模式

--- src/设计模式/代理模式/图片预加载/虚拟代理/index.js
+++ src/设计模式/代理模式/图片预加载/虚拟代理/index.js
@@ -9,7 +9,7 @@ const myImage = (() => {
 	}
 })()
 
-const loadingSrc = ''
+const loadingSrc = 'https://www.google.com.hk/url?sa=i&url=https%3A%2F%2Fwww.photophoto.cn%2Fpic%2F25781259.html&psig=AOvVaw03pqtji2QfV85ElmB0YoV3&ust=1622521840589000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCExYKL8_ACFQAAAAAdAAAAABAD'
 const imgSrc = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/fa98e67c4708449eb6894c7133d93774~tplv-k3u1fbpfcp-watermark.image'
 
 const proxyImage = (() => {
