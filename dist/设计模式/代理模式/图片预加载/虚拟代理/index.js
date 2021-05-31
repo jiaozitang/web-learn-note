87c68a1d1739cc89df697497159c21d7a2121fea
tangjiao12 <tangjiao11@jd.com>
2021-05-31 13:42:25 +0800

    设计模式

--- dist/设计模式/代理模式/图片预加载/虚拟代理/index.js
+++ dist/设计模式/代理模式/图片预加载/虚拟代理/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var _this = void 0;
-
 var myImage = function () {
   var imgNode = document.createElement('img');
   document.body.appendChild(imgNode);
@@ -12,14 +10,14 @@ var myImage = function () {
   };
 }();
 
-var loadingSrc = 'https://www.google.com.hk/url?sa=i&url=https%3A%2F%2Fwww.photophoto.cn%2Fpic%2F25781259.html&psig=AOvVaw03pqtji2QfV85ElmB0YoV3&ust=1622521840589000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCExYKL8_ACFQAAAAAdAAAAABAD';
+var loadingSrc = '../../../../img/loading.gif';
 var imgSrc = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/fa98e67c4708449eb6894c7133d93774~tplv-k3u1fbpfcp-watermark.image';
 
-var proxyImage = function proxyImage() {
+var proxyImage = function () {
   var img = new Image();
 
   img.onload = function () {
-    myImage.setSrc(_this.src);
+    myImage.setSrc(img.src);
   };
 
   return {
@@ -28,6 +26,6 @@ var proxyImage = function proxyImage() {
       img.src = src;
     }
   };
-};
+}();
 
 proxyImage.setSrc(imgSrc);
--- src/设计模式/代理模式/图片预加载/虚拟代理/index.js
+++ src/设计模式/代理模式/图片预加载/虚拟代理/index.js
@@ -9,13 +9,13 @@ const myImage = (() => {
 	}
 })()
 
-const loadingSrc = 'https://www.google.com.hk/url?sa=i&url=https%3A%2F%2Fwww.photophoto.cn%2Fpic%2F25781259.html&psig=AOvVaw03pqtji2QfV85ElmB0YoV3&ust=1622521840589000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCExYKL8_ACFQAAAAAdAAAAABAD'
+const loadingSrc = '../../../../img/loading.gif'
 const imgSrc = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/fa98e67c4708449eb6894c7133d93774~tplv-k3u1fbpfcp-watermark.image'
 
-const proxyImage = (() => {
+const proxyImage = (function () {
 	const img = new Image()
 	img.onload = () => {
-		myImage.setSrc(this.src)
+		myImage.setSrc(img.src)
 	}
 
 	return {
@@ -24,7 +24,7 @@ const proxyImage = (() => {
 			img.src = src
 		}
 	}
-})
+})()
 
 proxyImage.setSrc(imgSrc)
 
