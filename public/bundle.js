243f350c6a1f9c98aa78ff400e51d6cd94570127
tangjiao12 <tangjiao11@jd.com>
2021-06-02 12:42:58 +0800

    设计模式

---
+++ public/bundle.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  // src/foo.js
+  var foo = 'hello world!';
+
+  // src/main.js
+  function main () {
+    console.log(foo);
+  }
+
+  return main;
+
+}());
+//# sourceMappingURL=bundle.js.map
