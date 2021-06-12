b9d593ae306cdb603326ecf0bfb4d572b49e6c06
tangjiao12 <tangjiao11@jd.com>
2021-06-12 16:38:01 +0800

    设计模式

--- public/bundle.js
+++ public/bundle.js
@@ -1,24 +1,45 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    var before = function before(fn, beforeFn) {
-      return function () {
-        beforeFn.apply(this, arguments);
-        return fn.apply(this, arguments);
-      };
-    };
+  var Chain = function Chain(fn) {
+    this.fn = fn;
+    this.successor = null;
+  };
 
-    var ajax = function ajax(type, url, param) {
-      console.log(arguments); // ...ajax请求...省略
-    };
+  Chain.prototype.setNextSuccessor = function (successor) {
+    return this.successor = successor;
+  };
 
-    ajax = before(ajax, function (type, url, param) {
-      console.log(param);
-      param.token = 'xxx';
-    });
-    ajax('type', 'url', {
-      name: 'tj'
-    });
+  Chain.prototype.passRequest = function () {
+    var ret = this.fn.apply(this, arguments);
+
+    if (ret === 'nextSuccessor') {
+      return this.successor && this.successor.passRequest.apply(this.successor, arguments);
+    }
+
+    return ret;
+  };
+
+  var order500 = function order500(orderType) {
+    if (orderType === 500) {
+      console.log('已预付500定金，享有100优惠券');
+    } else {
+      return 'nextSuccessor';
+    }
+  };
+
+  var order200 = function order200(orderType) {
+    if (orderType === 200) {
+      console.log('已预付200定金，享有50元优惠券');
+    } else {
+      return 'nextSuccessor';
+    }
+  };
+
+  var chainOrder500 = new Chain(order500);
+  var chainOrder200 = new Chain(order200);
+  chainOrder500.setNextSuccessor(chainOrder200);
+  chainOrder500.passRequest(200);
 
 }());
 //# sourceMappingURL=bundle.js.map
