d4a270cf0d356aaea7d0ddef8e6f9c7601db5d6a
tangjiao12 <tangjiao11@jd.com>
2022-02-02 22:16:49 +0800

    init

--- src/手写系列/虚拟DOM/src/lib-test/const.js
+++
@@ -1,6 +0,0 @@
-export const PATCH_KEY = {
-    REPLACE: 0,
-    REORDER: 1,
-    PROPS: 2,
-    TEXT: 3
-}
--- src/设计模式/策略模式/表单校验/策略/index.js
+++
@@ -1,82 +0,0 @@
-/***********************策略对象**************************/
-
-let strategies = { 
-	isNonEmpty: function(value, errorMsg) { 
-		if (value === '') { 
-			return errorMsg; 
-		} 
-	},
-	 minLength: function(value, length, errorMsg) { 
-		if (value.length < length) { 
-			return errorMsg; 
-		} 
-	},
-	 isMobile: function(value, errorMsg) { 
-		if (!/(^1[3|5|8][0-9]{9}$)/.test(value)) { 
-			return errorMsg; 
-		} 
-	} 
-};  
-
-/***********************Validator类**************************/
-  
-let Validator = function() { 
-	this.cache = []; 
-};  
-
-Validator.prototype.add = function(dom, rules) {  
-	rules.forEach(rule => {
-		const { strategy, errorMsg } = rule
-		const [ strategyName, strategyCondition ] = strategy.split(':')
-		const { value } = dom
-		this.cache.push(strategies[strategyName].bind(dom, value, errorMsg))
-	})
-};  
-
-Validator.prototype.start = function() { 
-	let errorMsg
-	this.cache.some(cacheItem => {
-		const _errorMsg = cacheItem()
-		if (_errorMsg) {
-			errorMsg = _errorMsg
-			return true
-		} else {
-			return false
-		}
-	})
-	return errorMsg
-};  
-
-let validataFunc = () => {
-	let validator = new Validator()
-	validator.add(registerForm.userName, [{
-		strategy: 'isNonEmpty',
-		errorMsg: '用户名不能为空'
-	},{
-		strategy: 'minLength:10',
-	    errorMsg: '用户名长度不能小于10位'
-  	}]);
-  	validator.add(registerForm.password, [{
-  		strategy: 'minLength:6',
-    	errorMsg: '密码长度不能小于6位'
-  	}]);
-  	validator.add(registerForm.phoneNumber, [{
-  		strategy: 'isMobile',
-    	errorMsg: '手机号码格式不正确'
-  	}]);
-
-	const errorMsg = validator.start()
-	return errorMsg
-}
-/***********************客户调用代码**************************/
-  
-let registerForm = document.getElementById('registerForm');  
-
-
-registerForm.onsubmit = function() { 
-	let errorMsg = validataFunc();  
-	if (errorMsg) { 
-		alert(errorMsg); 
-		return false; 
-	} 
-};   
